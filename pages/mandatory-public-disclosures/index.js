efad32003f9efb547a9f75d2fc9228e3489a805e
preethi-benfy <preethikumar@benfy.co>
2022-11-03 17:10:19 +0530

    mandatory disclosure

--- pages/mandatory-public-disclosures/index.js
+++ pages/mandatory-public-disclosures/index.js
@@ -110,7 +110,7 @@ function MandatoryScreen() {
 
                     </div>
 
-                    <div className={'row' + ' ' + styles.mdBg}>
+                    <div className={'row pt60' + ' ' + styles.mdBg}>
 
                         <h4>Documents and Information</h4>
 
@@ -177,6 +177,243 @@ function MandatoryScreen() {
                         </Table> 
 
                         <p><b>NOTE:</b> THE SCHOOLS NEEDS TO UPLOAD THE SELF ATTESTED COPIES OF ABOVE LISTED DOCUMETNS BY CHAIRMAN/MANAGER/SECRETARY AND PRINCIPAL. IN CASE, IT IS NOTICED AT LATER STAGE THAT UPLOADED DOCUMENTS ARE NOT GENUINE THEN SCHOOL SHALL BE LIABLE FOR ACTION AS PER NORMS.</p>
+
+                    </div>
+
+                    <div className={'row pt60' + ' ' + styles.mdBg}>
+
+                        <h4>Results and Academics</h4>
+
+                        <Table bordered className={'mt-4' + ' ' + styles.tableborder}>
+                   
+                            <tbody>
+                                <tr className={styles.tableBorderColor}>
+                                <td>S.No</td>
+                                <td>DOCUMENTS/INFORMATION </td>
+                                <td>LINKS OF UPLOADED DOCUMENTS ON YOUR SCHOOL'S WEBSITE</td>
+                                </tr>
+
+                                <tr>
+                                <td>1</td>
+                                <td>FEE STRUCTURE OF THE SCHOOL*</td>
+                                <td><a href="#">Annexure 9</a></td>
+                                </tr>
+
+                                <tr>
+                                <td>2</td>
+                                <td>ANNUAL ACADEMIC CALENDER* </td>
+                                <td><a href="#">Annexure 10</a></td>
+                                </tr>
+
+                                <tr>
+                                <td>3</td>
+                                <td>LIST OF SCHOOL MANAGEMENT COMMITTEE (SMC)*</td>
+                                <td><a href="#">Annexure 11</a></td>
+                                </tr>
+
+                                <tr>
+                                <td>4</td>
+                                <td>LIST OF PARENTS TEACHERS ASSOCIATION (PTA) MEMBERS*</td>
+                                <td><a href="#">Annexure 12</a></td>
+                                </tr>
+
+                                <tr>
+                                <td>5</td>
+                                <td>LAST THREE-YEAR RESULT OF THE BOARD EXAMINATION AS PER APPLICABILITY</td>
+                                <td><a href="#">Annexure 13</a></td>
+                                </tr>
+
+                            </tbody>
+
+                        </Table> 
+
+                    </div>
+
+                    <div className={'row pt60' + ' ' + styles.mdBg}>
+
+                        <h4>Staff(Teaching)</h4>
+
+                        <Table bordered className={'mt-4' + ' ' + styles.tableborder}>
+                   
+                            <tbody>
+                                <tr className={styles.tableBorderColor}>
+                                <td>S.No</td>
+                                <td>INFORMATION</td>
+                                <td>DETAILS</td>
+                                </tr>
+
+                                <tr>
+                                <td>1</td>
+                                <td>PRINCIPAL*</td>
+                                <td>MRS PRIYANKA GHOSH JESURAJ</td>
+                                </tr>
+
+                                <tr>
+                                <td rowspan="4">2</td>
+                                <td >TOTAL NO. OF TEACHERS* </td>
+                                <td></td>
+                                </tr>
+
+                                <tr>
+                                <td >PGT*</td>
+                                <td></td>
+                                </tr>
+
+                                <tr>
+                                <td >TGT*</td>
+                                <td></td>
+                                </tr>
+
+                                <tr>
+                                <td >PRT*</td>
+                                <td></td>
+                                </tr>
+
+                                
+                                <tr>
+                                <td>3</td>
+                                <td>TEACHERS SECTION RATIO*</td>
+                                <td></td>
+                                </tr>
+
+                                <tr>
+                                <td>4</td>
+                                <td>DETAILS OF SPECIAL EDUCATOR*</td>
+                                <td></td>
+                                </tr>
+
+                                <tr>
+                                <td>5</td>
+                                <td>DETAILS OF COUNSELLOR AND WELLNESS TEACHER*</td>
+                                <td></td>
+                                </tr>
+
+                            </tbody>
+
+                        </Table> 
+                        
+                    </div>
+
+                    <div className={'row pt60'}>
+
+                        <h4>Result Class : X</h4>
+
+                        <Table bordered className={'mt-4' + ' ' + styles.tableborder}>
+                   
+                            <tbody>
+                                <tr className={styles.tableBorderColor}>
+                                <td>S.No</td>
+                                <td>YEAR </td>
+                                <td>NO. OF REGISTERED STUDENTS</td>
+                                <td>NO. OF STUDENTS PASSED</td>
+                                <td>PASS PERCENTAGE</td>
+                                <td>REMARKS</td>
+                                </tr>
+
+                                <tr>
+                                <td>1</td>
+                                <td></td>
+                                <td></td>
+                                <td></td>
+                                <td>EG. 78.56</td>
+                                <td></td>
+                                </tr>
+
+                            </tbody>
+
+                        </Table> 
+                        
+                    </div>
+
+                    <div className={'row pt60'}>
+
+                        <h4>Result Class : XII</h4>
+
+                        <Table bordered className={'mt-4' + ' ' + styles.tableborder}>
+                   
+                            <tbody>
+                                <tr className={styles.tableBorderColor}>
+                                <td>S.No</td>
+                                <td>YEAR </td>
+                                <td>NO. OF REGISTERED STUDENTS</td>
+                                <td>NO. OF STUDENTS PASSED</td>
+                                <td>PASS PERCENTAGE</td>
+                                <td>REMARKS</td>
+                                </tr>
+
+                                <tr>
+                                <td>1</td>
+                                <td></td>
+                                <td></td>
+                                <td></td>
+                                <td>1 EG. 78.56</td>
+                                <td></td>
+                                </tr>
+
+                            </tbody>
+
+                        </Table> 
+                        
+                    </div>
+
+                    <div className={'row pt60' + ' ' + styles.mdBg}>
+
+                        <h4>School and Infrastructure</h4>
+
+                        <Table bordered className={'mt-4' + ' ' + styles.tableborder}>
+                   
+                            <tbody>
+                                <tr className={styles.tableBorderColor}>
+                                <td>S.No</td>
+                                <td>INFORMATION </td>
+                                <td>DETAILS</td>
+                                </tr>
+
+                                <tr>
+                                <td>1</td>
+                                <td>TOTAL CAMPUS AREA OF THE SCHOOL (IN SQUARE MTR)*</td>
+                                <td></td>
+                                </tr>
+
+                                <tr>
+                                <td>2</td>
+                                <td>NO. AND SIZE OF THE CLASS ROOMS (IN SQ MTR)*</td>
+                                <td></td>
+                                </tr>
+
+                                <tr>
+                                <td>3</td>
+                                <td>NO. AND SIZE OF LABORATORIES INCLUDING COMPUTER LABS (IN SQ MTR)* </td>
+                                <td></td>
+                                </tr>
+
+                                <tr>
+                                <td>4</td>
+                                <td>INTERNET FACILITY*</td>
+                                <td></td>
+                                </tr>
+
+                                <tr>
+                                <td>5</td>
+                                <td>NO. OF GIRLS TOILETS*</td>
+                                <td></td>
+                                </tr>
+
+                                <tr>
+                                <td>6</td>
+                                <td>NO. OF BOYS TOILETS*</td>
+                                <td></td>
+                                </tr>
+
+                                <tr>
+                                <td>7</td>
+                                <td>LINK OF YOUTUBE VIDEO OF THE INSPECTION OF SCHOOL COVERING THE INFRASTRUCTURE OF THE SCHOOL</td>
+                                <td></td>
+                                </tr>
+
+                            </tbody>
+
+                        </Table> 
 
                     </div>
 
