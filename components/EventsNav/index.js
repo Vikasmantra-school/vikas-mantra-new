a4526d355f934fa4e02038c79cb1be60cc8cd863
preethi-benfy <preethikumar@benfy.co>
2022-11-04 18:25:40 +0530

    event dropdown

---
+++ components/EventsNav/index.js
@@ -0,0 +1,192 @@
+import React from 'react'
+import Nav from 'react-bootstrap/Nav';
+import Dropdown from 'react-bootstrap/Dropdown';
+import styles from "./style.module.css"
+
+function EventsNav() {
+  return (
+    <>
+
+    <Nav className="flex-column">
+
+    <Dropdown className={styles.dropdowns}>
+
+        <Dropdown.Toggle id="dropdown-basic">
+
+        <Nav.Item>
+            <Nav.Link className={styles.tabNavLink} eventKey="first">Beach Day</Nav.Link>
+            </Nav.Item>
+            
+        </Dropdown.Toggle>
+
+        <Dropdown.Menu className={styles.dropMenu}>
+            <Dropdown.Item>
+
+            <Nav.Item>
+                <Nav.Link className={styles.tabNavLink} eventKey="second">Be the Change</Nav.Link>
+            </Nav.Item>
+
+            </Dropdown.Item>
+
+            <Dropdown.Item>
+
+            <Nav.Item>
+                <Nav.Link className={styles.tabNavLink} eventKey="third">Best Emerging School</Nav.Link>
+            </Nav.Item>
+
+            </Dropdown.Item>
+
+            <Dropdown.Item>
+
+            <Nav.Item>
+                <Nav.Link className={styles.tabNavLink} eventKey="forth">Children&apos;s Day</Nav.Link>
+            </Nav.Item>
+
+            </Dropdown.Item>
+
+            <Dropdown.Item>
+
+                <Nav.Item>
+                    <Nav.Link className={styles.tabNavLink} eventKey="fifth">Friendship Day</Nav.Link>
+                </Nav.Item>
+
+            </Dropdown.Item>
+
+            <Dropdown.Item>
+
+                <Nav.Item>
+                    <Nav.Link className={styles.tabNavLink} eventKey="six">Girl Child Protection Day</Nav.Link>
+                </Nav.Item>
+
+            </Dropdown.Item>
+
+            <Dropdown.Item>
+
+                <Nav.Item>
+                    <Nav.Link className={styles.tabNavLink} eventKey="seven">Independence Day Celebrations</Nav.Link>
+                </Nav.Item>
+
+            </Dropdown.Item>
+
+            <Dropdown.Item>
+
+                <Nav.Item>
+                    <Nav.Link className={styles.tabNavLink} eventKey="eight">International Yoga Day Celebration</Nav.Link>
+                </Nav.Item>
+
+            </Dropdown.Item>
+
+            <Dropdown.Item>
+
+                <Nav.Item>
+                    <Nav.Link className={styles.tabNavLink} eventKey="nine">Investiture Ceremony</Nav.Link>
+                </Nav.Item>
+
+            </Dropdown.Item>
+
+            <Dropdown.Item>
+
+                <Nav.Item>
+                    <Nav.Link className={styles.tabNavLink} eventKey="ten">Janmashtami celebrations</Nav.Link>
+                </Nav.Item>
+
+            </Dropdown.Item>
+
+            <Dropdown.Item>
+
+                <Nav.Item>
+                    <Nav.Link className={styles.tabNavLink} eventKey="ten">Janmashtami celebrations</Nav.Link>
+                </Nav.Item>
+
+            </Dropdown.Item>
+
+            <Dropdown.Item>
+
+                <Nav.Item>
+                    <Nav.Link className={styles.tabNavLink} eventKey="eleven">KG Graduation</Nav.Link>
+                </Nav.Item>
+
+            </Dropdown.Item>
+
+            <Dropdown.Item>
+
+                <Nav.Item>
+                    <Nav.Link className={styles.tabNavLink} eventKey="twevle">National Sports Day</Nav.Link>
+                </Nav.Item>
+
+            </Dropdown.Item>
+
+            <Dropdown.Item>
+
+                <Nav.Item>
+                    <Nav.Link className={styles.tabNavLink} eventKey="thirteen">Newspaper Feature Article</Nav.Link>
+                </Nav.Item>
+
+            </Dropdown.Item>
+
+            <Dropdown.Item>
+
+                <Nav.Item>
+                    <Nav.Link className={styles.tabNavLink} eventKey="forteen">Science Expo</Nav.Link>
+                </Nav.Item>
+
+            </Dropdown.Item>
+
+            {/* <Dropdown.Item>
+
+                <Nav.Item>
+                    <Nav.Link className={styles.tabNavLink} eventKey="fifteen">Sense Booth</Nav.Link>
+                </Nav.Item>
+
+            </Dropdown.Item>
+
+            <Dropdown.Item>
+
+                <Nav.Item>
+                    <Nav.Link className={styles.tabNavLink} eventKey="sixteen">Skating Competiton</Nav.Link>
+                </Nav.Item>
+
+            </Dropdown.Item>
+
+            <Dropdown.Item>
+
+                <Nav.Item>
+                    <Nav.Link className={styles.tabNavLink} eventKey="seventeen">Student Excursion</Nav.Link>
+                </Nav.Item>
+
+            </Dropdown.Item>
+
+            <Dropdown.Item>
+
+                <Nav.Item>
+                    <Nav.Link className={styles.tabNavLink} eventKey="eighteen">The Journey</Nav.Link>
+                </Nav.Item>
+
+            </Dropdown.Item>
+
+            <Dropdown.Item>
+
+                <Nav.Item>
+                    <Nav.Link className={styles.tabNavLink} eventKey="nineteen">This School&apos;s Got Talent 2019</Nav.Link>
+                </Nav.Item>
+
+            </Dropdown.Item>
+
+            <Dropdown.Item>
+
+                <Nav.Item>
+                    <Nav.Link className={styles.tabNavLink} eventKey="twenty">Yellow Day</Nav.Link>
+                </Nav.Item>
+
+            </Dropdown.Item> */}
+        
+        </Dropdown.Menu>
+    </Dropdown>
+
+    </Nav>
+    
+    </>
+  )
+}
+
+export default EventsNav
--- pages/events/index.js
+++ pages/events/index.js
@@ -1,11 +1,10 @@
 import React from 'react'
 import styles from "./style.module.css"
-import Dropdown from 'react-bootstrap/Dropdown';
 import { Splide, SplideSlide } from '@splidejs/react-splide'
 import '@splidejs/react-splide/css'
-import Nav from 'react-bootstrap/Nav';
 import Tab from 'react-bootstrap/Tab';
 import Breadcrumb from '../../components/Breadcrumb/Breadcrumb'
+import EventsNav from '../../components/EventsNav';
 
 
 function index() {
@@ -29,41 +28,7 @@ function index() {
 
                     <h2>Events</h2>
 
-                    <Nav className="flex-column">
-
-                    <Dropdown className={styles.dropdowns}>
-
-                        <Dropdown.Toggle id="dropdown-basic">
-
-                        <Nav.Item>
-                            <Nav.Link className={styles.tabNavLink} eventKey="first">Beach Day</Nav.Link>
-                            </Nav.Item>
-                               
-                        </Dropdown.Toggle>
-
-                        <Dropdown.Menu className={styles.dropMenu}>
-                            <Dropdown.Item>
-
-                            <Nav.Item>
-                                <Nav.Link className={styles.tabNavLink} eventKey="second">Tab 2</Nav.Link>
-                            </Nav.Item>
-
-                            </Dropdown.Item>
-
-                            <li><hr className="dropdown-divider" /></li>
-
-                            <Dropdown.Item>
-
-                            <Nav.Item>
-                                <Nav.Link className={styles.tabNavLink} eventKey="second">Tab 2</Nav.Link>
-                            </Nav.Item>
-
-                            </Dropdown.Item>
-                        
-                        </Dropdown.Menu>
-                    </Dropdown>
-
-                    </Nav>
+                    <EventsNav />
 
                 </div>
 
@@ -148,9 +113,213 @@ function index() {
 
                             <div className='col-md-6'>
 
-                                <h2 className='ulineRed'>Beach Day</h2>
+                                <h2 className='ulineRed'>Launch of BE THE CHANGE</h2>
 
-                                <p className='pt-4'>We the tiny n happy fishes in the marine world. Today at Vikas Mantra Public School organized The Beach Day as a part of monthly concept. Children learnt the concept of going to beach in summer. Underwater creatures were taught through songs.</p>
+                                <p className='pt-4'>Oct 8, 2019 marked a historic day in the school&apos;s calendar by launching BE THE CHANGE – an initiative by the school joining hands with BE THE CHANGE to inculcate social responsibilities among children to bring out their innate talents. Chief Guest of the day – Actor and Celebrity Jeeva Ravi.</p>
+
+                            </div>
+
+                            <div className='col-md-6'>
+
+                                <div className='imgUp'>
+
+                                    <img src='/assets/events/beachday.png' alt='image' className='img-fluid' />
+
+                                </div>
+
+                            </div>
+                    
+                        </div>
+
+                    </div>
+
+                </section>
+
+                <section className={'greybg' + ' ' + 'pt60' + ' ' + 'pb60' + ' ' + styles.eventGallery}>
+
+                <div className='container'>
+
+                    <div className={'row' + ' ' + 'vmpsslide'}>
+                        <div className='col-md-12'>
+                            <Splide options={{
+                                rewind: true,
+                                gap: '1rem',
+                                perPage: 3,
+                                pagination: false,
+                                arrows: true,
+                                }}>
+
+                                <SplideSlide>
+
+                                    <img src='/assets/events/beachday.png' alt='image' className='img-fluid' />
+
+                                </SplideSlide>
+
+                                <SplideSlide>
+
+                                    <img src='/assets/events/beachday.png' alt='image' className='img-fluid' />
+
+                                </SplideSlide>
+
+                            </Splide>
+                        </div>
+                    </div>
+
+                </div>
+
+                </section>
+
+            </Tab.Pane>
+
+            <Tab.Pane eventKey="third">
+
+                <section className={'greybg' + ' ' + 'pt60' + ' ' + 'pb60'}>
+
+                    <div className='container'>
+
+                        <div className='row'>
+
+                            <div className='col-md-6'>
+
+                                <h2 className='ulineRed'>Best Emerging School Award</h2>
+
+                                <p className='pt-4'>We are happy to announce that our school Vikas Mantra Public school received the Indian icon of the year awards for Best Emerging School ( Holistic) of the year 2021 😀</p>
+
+                            </div>
+
+                            <div className='col-md-6'>
+
+                                <div className='imgUp'>
+
+                                    <img src='/assets/events/beachday.png' alt='image' className='img-fluid' />
+
+                                </div>
+
+                            </div>
+                    
+                        </div>
+
+                    </div>
+
+                </section>
+
+                <section className={'greybg' + ' ' + 'pt60' + ' ' + 'pb60' + ' ' + styles.eventGallery}>
+
+                <div className='container'>
+
+                    <div className={'row' + ' ' + 'vmpsslide'}>
+                        <div className='col-md-12'>
+                            <Splide options={{
+                                rewind: true,
+                                gap: '1rem',
+                                perPage: 3,
+                                pagination: false,
+                                arrows: true,
+                                }}>
+
+                                <SplideSlide>
+
+                                    <img src='/assets/events/beachday.png' alt='image' className='img-fluid' />
+
+                                </SplideSlide>
+
+                                <SplideSlide>
+
+                                    <img src='/assets/events/beachday.png' alt='image' className='img-fluid' />
+
+                                </SplideSlide>
+
+                            </Splide>
+                        </div>
+                    </div>
+
+                </div>
+
+                </section>
+
+            </Tab.Pane>
+
+            <Tab.Pane eventKey="forth">
+
+                <section className={'greybg' + ' ' + 'pt60' + ' ' + 'pb60'}>
+
+                    <div className='container'>
+
+                        <div className='row'>
+
+                            <div className='col-md-6'>
+
+                                <h2 className='ulineRed'>Children&apos;s Day at Raj Nivas</h2>
+
+                                <p className='pt-4'>On the eve of Childrens Day as a part of our Be The Change initiative the students of Vikas Mantra Public School were invited to Raj Nivas Puducherry to meet Lieutenant governor Dr Kiran Bedi. She encouraged the children and interacted with them. Indeed a memorable day in the life of students of VMPS.</p>
+
+                            </div>
+
+                            <div className='col-md-6'>
+
+                                <div className='imgUp'>
+
+                                    <img src='/assets/events/beachday.png' alt='image' className='img-fluid' />
+
+                                </div>
+
+                            </div>
+                    
+                        </div>
+
+                    </div>
+
+                </section>
+
+                <section className={'greybg' + ' ' + 'pt60' + ' ' + 'pb60' + ' ' + styles.eventGallery}>
+
+                <div className='container'>
+
+                    <div className={'row' + ' ' + 'vmpsslide'}>
+                        <div className='col-md-12'>
+                            <Splide options={{
+                                rewind: true,
+                                gap: '1rem',
+                                perPage: 3,
+                                pagination: false,
+                                arrows: true,
+                                }}>
+
+                                <SplideSlide>
+
+                                    <img src='/assets/events/beachday.png' alt='image' className='img-fluid' />
+
+                                </SplideSlide>
+
+                                <SplideSlide>
+
+                                    <img src='/assets/events/beachday.png' alt='image' className='img-fluid' />
+
+                                </SplideSlide>
+
+                            </Splide>
+                        </div>
+                    </div>
+
+                </div>
+
+                </section>
+
+            </Tab.Pane>
+
+            <Tab.Pane eventKey="fifth">
+
+                <section className={'greybg' + ' ' + 'pt60' + ' ' + 'pb60'}>
+
+                    <div className='container'>
+
+                        <div className='row'>
+
+                            <div className='col-md-6'>
+
+                                <h2 className='ulineRed'>Friendship Day Celebration</h2>
+
+                                <p className='pt-4'>Students of VMPS celebrated Friendship Day in school to show love and respect to their friends. They were engaged in the card making activity. The entire concept was to inculcate the value of friendship not only to school mates but also a special card was asked to be made to be given to their teacher or mother or father or even a helper at home.</p>
 
                             </div>
 
