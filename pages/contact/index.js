a4a8ecb4ae5e528ef8943cdb1bcb7471a6f78148
NasarLalu10 <112628963+NasarLalu10@users.noreply.github.com>
2022-11-04 12:10:40 +0530

    contact-page

--- pages/contact/index.js
+++ pages/contact/index.js
@@ -2,115 +2,100 @@ import styles from './style.module.css'
 import Breadcrumb from '../../components/Breadcrumb/Breadcrumb'
 
 const Contact = () => {
-  const pageTitle = 'Contact'
+    const pageTitle = 'Contact'
 
-  return (
-    <>
-      <Breadcrumb pageName={pageTitle} />
+    return (
+        <>
+            <Breadcrumb pageName={pageTitle} />
 
-      <section className='pt-5 '>
-        <div className='container position-relative'>
-          <div className='row py-5 dot-vector-before red-dot'>
-            <div className='col-sm-8 col-md-6'>
-              <h2 className={styles.headingPdng}>Contact</h2>
-            </div>
-          </div>
-          <img
-            src='assets/campus/school-building3.png'
-            alt='image'
-            className={styles.schlBusImg + ' ' + ' img-fluid'}
-          />
-        </div>
-      </section>
+            <section className='pt-5 '>
+                <div className='container position-relative'>
 
-      <section
-        className={
-          styles.sectionPdng +
-          ' greybg position-relative ' +
-          styles.vectorImg10 +
-          ' ' +
-          styles.vectorImg13
-        }
-      >
-        <div className='container'>
-          <div className='row'>
-            <div className='col-md-5 position-relative'></div>
+                    <div className='row py-5 dot-vector-before red-dot'>
+                        <div className='col-sm-8 col-md-6'>
+                            <h2 className={styles.headingPdng}>Contact</h2>
+                        </div>
+                    </div>
 
-            <div className='col-md-7 position-relative'></div>
-          </div>
-        </div>
-      </section>
-      <section className='greybg pt60 pb60'>
-        <div className='container'>
-          <div className={styles.rowPdng + ' row text-center'}>
-            <div className='col-md-4'>
-              <div className='position-relative'>
-                {/* <img src='/assets/vectors/vector13.png' alt="image" className={styles.vectorImg13} /> */}
-
-                <img
-                  src='/assets/icons/mail.png'
-                  alt='image'
-                  className={styles.iconsImg}
-                />
-
-                <h4 className='py-4'>Postal address</h4>
-
-                <div className={styles.paraAlign}>
-                  <p>
-                    S.No:168,168/3,
-                    <br />
-                    Off Mahindra World City,
-                    <br />
-                    Anjur - 603204, Chengalpattu
-                  </p>
                 </div>
-              </div>
-            </div>
 
-            <div className='col-md-4'>
-              <img
-                src='/assets/icons/call.png'
-                alt='image'
-                className={styles.iconsImg}
-              />
+                <img src='assets/campus/school-building3.png' alt='image' className={styles.schlBusImg + ' ' + ' img-fluid'} />
 
-              <h4 className='py-4'>Phone & Email</h4>
+            </section>
 
-              <div className={styles.paraAlign}>
-                <p>info@vikasmantra.org</p>
-                <p>
-                  <a href='tel:+91 - 7092 450 450'>+91 - 7092 450 450</a>
-                </p>
-                <p>
-                  <a href='tel:+91 - 8148 143 834'>+91 - 8148 143 834</a>
-                </p>
-              </div>
-            </div>
+            <section className={styles.sectionPdng + ' greybg position-relative ' + ' ' +
+                styles.vectorImg13}>
+                <div className='container'>
+                    <div className='row'>
+                        <div className='col-md-5 position-relative'></div>
 
-            <div className='col-md-4 position-relative'>
-              <img
-                src='/assets/icons/time.png'
-                alt='image'
-                className={styles.iconsImg}
-              />
+                        <div className='col-md-7 position-relative'></div>
+                    </div>
+                </div>
+            </section>
 
-              <h4 className='py-4'>Office hours</h4>
 
-              <div className={styles.paraAlign3}>
-                <p>Monday - Friday</p>
-                <p> 9.00 am - 4.00 pm</p>
+            <section className= { ' greybg pt60 pb60 position-relative ' + ' ' + styles.vectorImg10}>
+                <div className='container'>
+                    <div className={styles.rowPdng + ' row text-center'}>
 
-                <p> Saturday</p>
-                <p> 9.00 am - 1.00 pm</p>
-              </div>
+                        <div className= {'col-md-4 ' + ' ' + styles.mobColPdng }>
+                            <div className='position-relative'>
+                                {/* <img src='/assets/vectors/vector13.png' alt="image" className={styles.vectorImg13} /> */}
 
-              {/* <img src='/assets/vectors/vector10.png' alt="image" className={styles.vectorImg10} /> */}
-            </div>
-          </div>
-        </div>
-      </section>
-    </>
-  )
+                                <img src='/assets/icons/mail.png' alt='image' className={styles.iconsImg} />
+
+                                <h4 className='py-4'>Postal address</h4>
+
+                                <div className={styles.paraAlign}>
+                                    <p>
+                                        S.No:168,168/3,
+                                        <br />
+                                        Off Mahindra World City,
+                                        <br />
+                                        Anjur - 603204, Chengalpattu
+                                    </p>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div className={'col-md-4 ' + ' ' + styles.mobColPdng}>
+                            <img src='/assets/icons/call.png' alt='image' className={styles.iconsImg} />
+
+                            <h4 className='py-4'>Phone & Email</h4>
+
+                            <div className={styles.paraAlign}>
+                                <p>info@vikasmantra.org</p>
+                                <p>
+                                    <a href='tel:+91 - 7092 450 450'>+91 - 7092 450 450</a>
+                                </p>
+                                <p>
+                                    <a href='tel:+91 - 8148 143 834'>+91 - 8148 143 834</a>
+                                </p>
+                            </div>
+                        </div>
+
+                        <div className={'col-md-4 position-relative ' + ' ' + styles.mobColPdng} >
+                            <img src='/assets/icons/time.png' alt='image' className={styles.iconsImg} />
+
+                            <h4 className='py-4'>Office hours</h4>
+
+                            <div className={styles.paraAlign3}>
+                                <p>Monday - Friday</p>
+                                <p> 9.00 am - 4.00 pm</p>
+
+                                <p> Saturday</p>
+                                <p> 9.00 am - 1.00 pm</p>
+                            </div>
+
+                        </div>
+
+                    </div>
+                </div>
+            </section>
+
+        </>
+    )
 }
 
 export default Contact
