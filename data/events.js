9b77ca8456ddce5d0abb6ecd81bde8152229e2bb
renesundar <renesundar@benfy.co>
2022-11-09 13:56:35 +0530

    events

---
+++ data/events.js
@@ -0,0 +1,26 @@
+export const events = [
+  {
+    id: 'first',
+    dropdown: 'Beach Day',
+    title: 'Beach Day for Kg',
+    desc: 'We the tiny n happy fishes in the marine world.',
+    mainImage: '/assets/events/beach-day/1.jpeg',
+    gallery: [
+      '/assets/events/beach-day/1.jpeg',
+      '/assets/events/beach-day/2.jpeg',
+      '/assets/events/beach-day/3.jpeg',
+    ],
+  },
+  {
+    id: 'second',
+    id: 'Bad Day',
+    title: 'Bad Day for Kg',
+    desc: 'We the tiny n happy fishes in the marine world.',
+    mainImage: '/assets/events/beach-day/1.jpeg',
+    gallery: [
+      '/assets/events/beach-day/1.jpeg',
+      '/assets/events/beach-day/2.jpeg',
+      '/assets/events/beach-day/3.jpeg',
+    ],
+  },
+]
