e379cbdf26fbb3b09ebf2f42368bc7bdd72e5a0e
NasarLalu10 <112628963+NasarLalu10@users.noreply.github.com>
2022-11-18 14:48:48 +0530

    events-check

--- pages/events/index.js
+++ pages/events/index.js
@@ -17,7 +17,7 @@ const Events = () => {
   const pageTitle = 'Events'
 
   //gsap-integration
-  let title = useRef(null)
+  // let title = useRef(null)
   function staggerAnimeFunc(elem, index) {
     let text = elem.querySelectorAll('.bottomToTop')
     let imageAnime = elem.querySelectorAll('.leftToRight')
@@ -63,35 +63,35 @@ const Events = () => {
     )
   }
 
-  function titleAnime() {
-    gsap.fromTo(
-      title,
-      { opacity: 0 },
-      {
-        duration: 1.2,
-        opacity: 1,
-        y: -20,
-        ease: Power3.easeOut,
-      }
-    )
-  }
+  // function titleAnime() {
+  //   gsap.fromTo(
+  //     title,
+  //     { opacity: 0 },
+  //     {
+  //       duration: 1.2,
+  //       opacity: 1,
+  //       y: -20,
+  //       ease: Power3.easeOut,
+  //     }
+  //   )
+  // }
 
   useEffect(() => {
     const parentTrigger = document.querySelectorAll('.parentAnimeStarts')
     parentTrigger.forEach(staggerAnimeFunc)
-    titleAnime()
+    // titleAnime()
   }, [])
 
   return (
     <>
       <Breadcrumb pageName={pageTitle} />
 
-      <section className='pt60 bottomToTop parentAnimeStarts'>
+      <section className='pt60 parentAnimeStarts'>
         <div className={'container-fluid' + ' ' + styles.tabSpace}>
           <Tab.Container id='left-tabs-example' defaultActiveKey='first'>
             <div className='row pt-5'>
               <div className={'col-md-5' + ' ' + styles.eventText}>
-                <h2>Events</h2>
+                <h2 className='bottomToTop'>Events</h2>
                 <EventsNav events={events} />
               </div>
             </div>
@@ -100,12 +100,11 @@ const Events = () => {
               return (
                 <Tab.Content key={index}>
                   <Tab.Pane eventKey={data.id}>
-                    <section
-                      className='whitebg pt60 pb60 parentAnimeStarts '>
+                    <section className='whitebg pt60 pb60 parentAnimeStarts '>
                       <div className='container'>
                         <div className='row'>
                           <div className='col-md-6'>
-                            <h2 className='ulineRed'>{data.title} </h2>
+                            <h2 className='ulineRed bottomToTop '>{data.title} </h2>
 
                             <p className='pt-4 bottomToTop '>{data.desc}</p>
                           </div>
@@ -123,12 +122,10 @@ const Events = () => {
                       </div>
                     </section>
 
-                    <section
-                      className={' greybg  VectorSpiralPink pt80 pb80 parentAnimeStarts ' + styles.eventGallery + ' ' + styles.eventSectionPdng}
-                    >
+                    <section className={' greybg  VectorSpiralPink pt80 pb80 parentAnimeStarts ' + styles.eventGallery + ' ' + styles.eventSectionPdng} >
                       <div className='container'>
                         <div className='row vmpsslide'>
-                          <div className='col-md-12 bottomToTop'>
+                          <div className='col-md-12'>
                             <Splide
                               options={{
                                 rewind: true,
@@ -147,7 +144,7 @@ const Events = () => {
                               {data.gallery?.map((data, index) => {
                                 return (
                                   <SplideSlide
-                                    className='eventGallery'
+                                    className='eventGallery leftToRight'
                                     key={index}
                                   >
                                     <img
