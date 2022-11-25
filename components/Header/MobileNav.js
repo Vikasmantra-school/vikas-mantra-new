a12288867449f42a54e2904f0b8ce3d9d4e82c3e
renesundar <renesundar@benfy.co>
2022-11-25 18:26:27 +0530

    Update MobileNav.js

--- components/Header/MobileNav.js
+++ components/Header/MobileNav.js
@@ -15,10 +15,10 @@ const MobileNav = () => {
 
     router.events.on('routeChangeStart', handleRouteChange)
 
-    // return () => {
-    //   router.events.off('routeChangeStart', handleRouteChange)
-    // }
-  }, [])
+    return () => {
+      router.events.off('routeChangeStart', handleRouteChange)
+    }
+  }, [router.events])
 
   function navLink(e) {
     const sibling = e.target.nextSibling
