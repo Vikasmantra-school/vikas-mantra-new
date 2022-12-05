e501c5644b4b411fc27ae3a622d49f7ce1ba9517
NasarLalu10 <112628963+NasarLalu10@users.noreply.github.com>
2022-12-05 12:58:03 +0530

    after-animation-integration

--- pages/_app.js
+++ pages/_app.js
@@ -5,9 +5,13 @@ import Nav from '../components/Header/Nav'
 import { Footer } from '../components/Footer/Footer'
 import { gsap } from 'gsap'
 import { ScrollTrigger } from 'gsap/dist/ScrollTrigger'
+import { CSSRulePlugin } from 'gsap/dist/CSSRulePlugin'
 import { Analytics } from '@vercel/analytics/react';
 
+
+gsap.registerPlugin(CSSRulePlugin)
 function MyApp({ Component, pageProps }) {
+
   function Anime() {
     // const text = new SplitType('.AnimeStarts', { types: 'lines' })
 
@@ -20,6 +24,119 @@ function MyApp({ Component, pageProps }) {
     // function eachDiv(elem) {
     //   elem.classList.add('AnimeElement')
     // }
+
+
+    const parentTrigger2 = document.querySelectorAll('.AnimeStarts')
+    parentTrigger2.forEach(fadeInAfter)
+    function fadeInAfter(elem) {
+
+      const rule = CSSRulePlugin.getRule(".imgBgColor:after");
+      gsap.fromTo(
+        rule,
+        {
+          opacity: 0,
+          y: 50,
+        },
+        {
+          duration: 5,
+          delay: 0.5,
+          y: 0,
+          cssRule: {
+            opacity: "1",
+          }
+        });
+
+      const rule2 = CSSRulePlugin.getRule(".imgBgColor2:after");
+      gsap.fromTo(
+        rule2,
+        {
+          opacity: 0,
+          y: 50,
+        },
+        {
+          duration: 4,
+          y: 0,
+          cssRule: {
+            opacity: "1",
+          }
+        });
+
+      const rule3 = CSSRulePlugin.getRule(".imgBgColorLeft:after");
+      gsap.fromTo(
+        rule3,
+        {
+          opacity: 0,
+          y: 50,
+        },
+        {
+          duration: 4,
+          y: 0,
+          cssRule: {
+            opacity: "1",
+          }
+        });
+
+      const rule4 = CSSRulePlugin.getRule(".imgBgColorAdmission1:after");
+      gsap.fromTo(
+        rule4,
+        {
+          opacity: 0,
+          y: 50,
+        },
+        {
+          duration: 4,
+          y: 0,
+          cssRule: {
+            opacity: "1",
+          }
+        });
+
+      const rule5 = CSSRulePlugin.getRule(".imgBgColorAdmission2:after");
+      gsap.fromTo(
+        rule5,
+        {
+          opacity: 0,
+          y: 50,
+        },
+        {
+          duration: 4,
+          y: 0,
+          cssRule: {
+            opacity: "1",
+          }
+        });
+
+      const rule6 = CSSRulePlugin.getRule(".imgBgColorBlog1:after");
+      gsap.fromTo(
+        rule6,
+        {
+          opacity: 0,
+          y: 50,
+        },
+        {
+          duration: 4,
+          y: 0,
+          cssRule: {
+            opacity: "1",
+          }
+        });
+
+      const rule7 = CSSRulePlugin.getRule(".imgBgColorBlog2:after");
+      gsap.fromTo(
+        rule7,
+        {
+          opacity: 0,
+          y: 50,
+        },
+        {
+          duration: 4,
+          y: 0,
+          cssRule: {
+            opacity: "1",
+          }
+        });
+
+    }
 
     const parentTrigger = document.querySelectorAll('.AnimeStarts')
     parentTrigger.forEach(fadeIn)
@@ -83,6 +200,7 @@ function MyApp({ Component, pageProps }) {
     import('../node_modules/bootstrap/dist/js/bootstrap')
   }, [Anime])
   gsap.registerPlugin(ScrollTrigger)
+  gsap.registerPlugin(CSSRulePlugin)
 
   return (
     <>
