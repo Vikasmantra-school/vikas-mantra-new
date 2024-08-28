86381cd1bc4f924f96a1aa601a8cacfafa66d070
benfy-dev <139740098+sneha-benfy@users.noreply.github.com>
2024-08-28 12:37:13 +0530

    Added FAQ and Fixed bug in about
    Added FAQ in About us page
    Fixed a bug where the website is overflowing

--- pages/about/index.js
+++ pages/about/index.js
@@ -1,9 +1,12 @@
 import styles from './style.module.css'
-import { Breadcrumb } from '../../components/Breadcrumb/Breadcrumb'
+import { Breadcrumb } from '../../components/Breadcrumb/Breadcrumb';
+import { gsap } from 'gsap';
+import { ScrollTrigger } from 'gsap/dist/ScrollTrigger';
 
 const About = () => {
 
     const pageTitle = 'About'
+
 
     return (
 
@@ -11,7 +14,7 @@ const About = () => {
 
             <Breadcrumb pageName={pageTitle} />
 
-            <section className='pt-4'>
+            <section className='pt-4 '>
 
                 <div className='container position-relative'>
 
@@ -59,7 +62,7 @@ const About = () => {
 
                                     <picture>
                                         <source srcSet='/assets/banners/school-building.webp' />
-                                        <source srcSet='/assets/banners/school-building.png'/>
+                                        <source srcSet='/assets/banners/school-building.png' />
                                         <img src='/assets/banners/school-building.png' alt="image" className={styles.ResponsiveGroupImage + ' AnimeElement'} />
                                     </picture>
 
@@ -105,6 +108,160 @@ const About = () => {
                 </div>
 
             </section>
+
+            <section className="faq">
+                <div className="container">
+                    <div className="row">
+                        <div className='AnimeStarts pt-5'>
+                            <h2 className='ulineRed AnimeElement'>FAQ's</h2>
+                        </div>
+                        <div className="col-md-10 AnimeStarts">
+                            <div className={'accordion py-5 AnimeElement' + styles.accordionExample} id='accordionExample' >
+
+                                <div className='accordion-item'>
+
+                                    <h2 className='accordion-header' id='headingZero'>
+                                        <button
+                                            className='accordion-button'
+                                            type='button'
+                                            data-bs-toggle='collapse'
+                                            data-bs-target='#collapseZero'
+                                            aria-expanded='false'
+                                            aria-controls='collapseZero'
+                                        >
+                                            <h5 className='pt-3 AdmissionScrollAnimeElement'>
+                                                How does your school keep the CBSE curriculum relevant and engaging?
+                                            </h5>
+                                        </button>
+                                    </h2>
+
+                                    <div
+                                        id='collapseZero'
+                                        className='accordion-collapse collapse show'
+                                        aria-labelledby='headingZero'
+                                        data-bs-parent='#accordionExample'
+                                    >
+                                        <div className='accordion-body'>
+                                            <p className='AdmissionScrollAnimeElement'>
+                                                We believe that at Vikas Mantra, your child will acquire academic knowledge and develop essential life skills that will serve them well throughout their journey.
+                                            </p>
+                                        </div>
+                                    </div>
+                                </div>
+
+                                <div className='accordion-item'>
+                                    <h2 className='accordion-header' id='headingOne'>
+                                        <button
+                                            className='accordion-button collapsed'
+                                            type='button'
+                                            data-bs-toggle='collapse'
+                                            data-bs-target='#collapseOne'
+                                            aria-expanded='true'
+                                            aria-controls='collapseOne'
+                                        >
+                                            <h5 className='pt-3 AdmissionScrollAnimeElement'>
+                                                What unique teaching methods cater to diverse learning styles in your school?
+                                            </h5>
+                                        </button>
+                                    </h2>
+                                    <div
+                                        id='collapseOne'
+                                        className='accordion-collapse collapse'
+                                        aria-labelledby='headingOne'
+                                        data-bs-parent='#accordionExample'
+                                    >
+                                        <div className='accordion-body AdmissionScrollAnimeElement'>
+                                            Vikas Mantra is committed to providing a nurturing environment where every child can thrive. We offer unique teaching methods and support programs to ensure your child's holistic development.
+                                        </div>
+                                    </div>
+                                </div>
+
+                                <div className='accordion-item'>
+                                    <h2 className='accordion-header' id='headingTwo'>
+                                        <button
+                                            className='accordion-button collapsed'
+                                            type='button'
+                                            data-bs-toggle='collapse'
+                                            data-bs-target='#collapseTwo'
+                                            aria-expanded='false'
+                                            aria-controls='collapseTwo'
+                                        >
+                                            <h5 className='pt-3 AdmissionScrollAnimeElement'>
+                                                What programs support student well-being in your CBSE school?
+                                            </h5>
+                                        </button>
+                                    </h2>
+                                    <div
+                                        id='collapseTwo'
+                                        className='accordion-collapse collapse'
+                                        aria-labelledby='headingTwo'
+                                        data-bs-parent='#accordionExample'
+                                    >
+                                        <div className='accordion-body AdmissionScrollAnimeElement'>
+                                            Vikas Mantra prioritises student well-being. To support holistic development, we offer counselling, mental health programs, and activities.
+                                        </div>
+                                    </div>
+                                </div>
+
+                                <div className='accordion-item'>
+                                    <h2 className='accordion-header' id='headingThree'>
+                                        <button
+                                            className='accordion-button collapsed'
+                                            type='button'
+                                            data-bs-toggle='collapse'
+                                            data-bs-target='#collapseThree'
+                                            aria-expanded='false'
+                                            aria-controls='collapseThree'
+                                        >
+                                            <h5 className='pt-3 AdmissionScrollAnimeElement'>
+                                                How does your school ensure an inclusive environment for all students?
+                                            </h5>
+                                        </button>
+                                    </h2>
+                                    <div
+                                        id='collapseThree'
+                                        className='accordion-collapse collapse'
+                                        aria-labelledby='headingThree'
+                                        data-bs-parent='#accordionExample'
+                                    >
+                                        <div className='accordion-body AdmissionScrollAnimeElement'>
+                                            Vikas Mantra is committed to fostering an inclusive environment where all students feel valued and supported. We have implemented various initiatives to ensure this.
+                                        </div>
+                                    </div>
+                                </div>
+
+                                <div className='accordion-item'>
+                                    <h2 className='accordion-header' id='headingFour'>
+                                        <button
+                                            className='accordion-button collapsed'
+                                            type='button'
+                                            data-bs-toggle='collapse'
+                                            data-bs-target='#collapseFour'
+                                            aria-expanded='false'
+                                            aria-controls='collapseFour'
+                                        >
+                                            <h5 className='pt-3 AdmissionScrollAnimeElement'>
+                                                How does your school use technology to enhance learning in the CBSE curriculum?
+                                            </h5>
+                                        </button>
+                                    </h2>
+                                    <div
+                                        id='collapseFour'
+                                        className='accordion-collapse collapse'
+                                        aria-labelledby='headingFour'
+                                        data-bs-parent='#accordionExample'
+                                    >
+                                        <div className='accordion-body AdmissionScrollAnimeElement'>
+                                            At Vikas Mantra, we use technology for modern learning, providing digital resources, interactive experiences, coding skills, and digital literacy training to equip students for the future.
+                                        </div>
+                                    </div>
+                                </div>
+
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section >
 
             <section className={styles.bannerSection + ' d-none d-lg-flex' + ' ' + styles.vectorImg4}>
 
