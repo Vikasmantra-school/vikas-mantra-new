e7561848cb0058b63c4bb8f9d9ec2c2f5c4ee848
Eabishenbenfy <renesundar@benfy.co>
2025-02-26 14:02:32 +0530

    Popup Update

--- components/Popup/PopupModal.js
+++ components/Popup/PopupModal.js
@@ -1,54 +1,64 @@
-import React, { useState, useEffect } from 'react';
-import Modal from 'react-bootstrap/Modal';
-import Image from 'next/image';
-import PopupImageDesktop from '../../public/assets/Popups/mindfull.jpg';
-import PopupImageMobile from '../../public/assets/Popups/maambakkam-mobile-popup.png';
-import { useRouter } from 'next/router';
-import styles from './style.module.css';
+import React, { useState, useEffect } from "react";
+import Modal from "react-bootstrap/Modal";
+import Image from "next/image";
+import PopupImageDesktop from "../../public/assets/Popups/maambakkam-desktop-popup.png";
+import PopupImageMobile from "../../public/assets/Popups/maambakkam-mobile-popup.png";
+import { useRouter } from "next/router";
+import styles from "./style.module.css";
 
 const PopupModal = () => {
-    const router = useRouter();
+  const router = useRouter();
 
-    const [show, setShow] = useState(false);
+  const [show, setShow] = useState(false);
 
-    const handleClose = () => {
-        setShow(false);
-        localStorage.setItem('Name', false);
-    };
+  const handleClose = () => {
+    setShow(false);
+    localStorage.setItem("Name", false);
+  };
 
-    const handleShow = () => {
-        setShow(true);
-        localStorage.setItem('Name', true);
-    };
+  const handleShow = () => {
+    setShow(true);
+    localStorage.setItem("Name", true);
+  };
 
-    const handleRedirect = () => {
-        router.push('/mambakkam');
-    };
+  const handleRedirect = () => {
+    router.push("/mambakkam");
+  };
 
-    useEffect(() => {
-        handleShow();
-        localStorage.setItem('Name', show);
-    }, []);
+  useEffect(() => {
+    handleShow();
+    localStorage.setItem("Name", show);
+  }, []);
 
-    return (
-        <>
-            <Modal show={show} onHide={handleClose} animation={true} dialogClassName={styles.customDialog}  contentClassName={styles.customContent}>
-                <Modal.Header closeButton></Modal.Header>
-                <Modal.Body onClick={handleRedirect} style={{ cursor: 'pointer' }}>
-                    <div>
-                        {/* Desktop Image */}
-                        <div className={styles.desktopImage}>
-                            <Image className={styles.deskimg} src={PopupImageDesktop} alt="popup-desktop" />
-                        </div>
-                        {/* Mobile Image */}
-                        <div className={styles.mobileImage} >
-                            <Image src={PopupImageDesktop} alt="popup-mobile" />
-                        </div>
-                    </div>
-                </Modal.Body>
-            </Modal>
-        </>
-    );
+  return (
+    <>
+      <Modal
+        show={show}
+        onHide={handleClose}
+        animation={true}
+        dialogClassName={styles.customDialog}
+        contentClassName={styles.customContent}
+      >
+        <Modal.Header closeButton></Modal.Header>
+        <Modal.Body onClick={handleRedirect} style={{ cursor: "pointer" }}>
+          <div>
+            {/* Desktop Image */}
+            <div className={styles.desktopImage}>
+              <Image
+                className={styles.deskimg}
+                src={PopupImageDesktop}
+                alt="popup-desktop"
+              />
+            </div>
+            {/* Mobile Image */}
+            <div className={styles.mobileImage}>
+              <Image src={PopupImageMobile} alt="popup-mobile" />
+            </div>
+          </div>
+        </Modal.Body>
+      </Modal>
+    </>
+  );
 };
 
 export default PopupModal;
