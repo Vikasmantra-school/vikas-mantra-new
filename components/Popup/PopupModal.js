078efe8afcabe8c501b4d4f1ddddee6f30c7ba27
Eabishenbenfy <37442152+renesundar@users.noreply.github.com>
2025-05-13 19:01:50 +0530

    updated popup Banner

--- components/Popup/PopupModal.js
+++ components/Popup/PopupModal.js
@@ -1,8 +1,9 @@
 import React, { useState, useEffect } from "react";
 import Modal from "react-bootstrap/Modal";
 import Image from "next/image";
-import PopupImageDesktop from "../../public/assets/Popups/maambakkam-desktop-popup.png";
-import PopupImageMobile from "../../public/assets/Popups/maambakkam-mobile-popup.png";
+import PopupImageDesktop from "../../public/assets/Popups/topper-desk.png";
+// import PopupImageMobile from "../../public/assets/Popups/maambakkam-mobile-popup.png";
+import PopupImage from "../../public/assets/Popups/topper.jpg";
 import { useRouter } from "next/router";
 import styles from "./style.module.css";
 
@@ -52,7 +53,7 @@ const PopupModal = () => {
             </div>
             {/* Mobile Image */}
             <div className={styles.mobileImage}>
-              <Image src={PopupImageMobile} alt="popup-mobile" />
+              <Image src={PopupImage} alt="popup-mobile" />
             </div>
           </div>
         </Modal.Body>
