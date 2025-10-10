a4d8c798707e1091dbfce344f84f47a675347dee
mathuseelan-benfy <mathuseelan@benfy.co>
2025-10-10 16:31:21 +0530

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -146,8 +146,8 @@ function Home() {
                   options={{
                     spacing: 0,
                     type: "loop",
-                    // pauseOnHover: true,
-                    // pauseOnFocus: false,
+                    pauseOnHover: true,
+                    pauseOnFocus: false,
                     pagination: true,
                     arrows: false,
                     autoplay: true,
@@ -165,7 +165,7 @@ function Home() {
                           alt="Image 1"
                         />
                       </div>
-                      {/* <div className={styles.sliderContentWrapper}>
+                      <div className={styles.sliderContentWrapper}>
                         <h2>
                           Success in wanted Steps,Excellence in Every Lesson
                         </h2>
@@ -177,7 +177,7 @@ function Home() {
                             color: "black",
                           }}
                         />
-                      </div> */}
+                      </div>
                     </div>
                   </SplideSlide>
                   <SplideSlide>
@@ -189,7 +189,7 @@ function Home() {
                           alt="Image 1"
                         />
                       </div>
-                      {/* <div className={styles.sliderContentWrapper}>
+                      <div className={styles.sliderContentWrapper}>
                         <h2>
                           Success in wanted Steps,Excellence in Every Lesson
                         </h2>
@@ -201,14 +201,14 @@ function Home() {
                             color: "black",
                           }}
                         />
-                      </div> */}
+                      </div>
                     </div>
                   </SplideSlide>
                 </Splide>
               </div>
             </Col>
 
-            <Col className="d-none d-md-block" md={6}>
+            {/* <Col className="d-none d-md-block" md={6}>
               <div
                 className={styles.bannerSideWrapper}
                 style={{ backgroundColor: "#FFEBEB" }}>
@@ -239,9 +239,9 @@ function Home() {
                   </Link>
                 </div>
               </div>
-            </Col>
+            </Col> */}
 
-            <Col className="d-none d-md-block" md={6}>
+            {/* <Col className="d-none d-md-block" md={6}>
               <div
                 className={styles.bannerSideWrapper}
                 style={{
@@ -272,7 +272,7 @@ function Home() {
                   </Link>
                 </div>
               </div>
-            </Col>
+            </Col> */}
             <Col md={6} className="d-none d-md-block">
               <div className="overflow-hidden">
                 <Splide
@@ -300,7 +300,7 @@ function Home() {
                           alt="Image 1"
                         />
                       </div>
-                      {/* <div className={styles.sliderContentWrapper}>
+                      <div className={styles.sliderContentWrapper}>
                         <h2>
                           Every child’s growth is guided with meaning and
                           momentum.
@@ -313,7 +313,7 @@ function Home() {
                             color: "white",
                           }}
                         />
-                      </div> */}
+                      </div>
                     </div>
                   </SplideSlide>
                   <SplideSlide>
@@ -325,7 +325,7 @@ function Home() {
                           alt="Image 1"
                         />
                       </div>
-                      {/* <div className={styles.sliderContentWrapper}>
+                      <div className={styles.sliderContentWrapper}>
                         <h2>
                           Every child’s growth is guided with meaning and
                           momentum.
@@ -338,7 +338,7 @@ function Home() {
                             color: "white",
                           }}
                         />
-                      </div> */}
+                      </div>
                     </div>
                   </SplideSlide>
                 </Splide>
